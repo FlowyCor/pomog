015d6ef769d7c1fddab1255a2b558bbaa843eb62
Fawion <95681272+FlowyCor@users.noreply.github.com>
2024-02-14 01:22:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const buttonYes = document.querySelector('.yes');
+const buttonNo = document.querySelector('.no');
+
+let buttonYesFontSize = 2;
+let buttonNoFontSize = 2;
+
+buttonNo.addEventListener('click', doYesMore);
+buttonYes.addEventListener('click', buttonYesPushed);
+
+function doYesMore () {
+    buttonYesFontSize += 0.4;
+    buttonYes.style.fontSize = buttonYesFontSize + 'em';
+    buttonNoFontSize -= 0.1;
+    buttonNo.style.fontSize = buttonNoFontSize + 'em'
+}
+
+function buttonYesPushed() {
+    location.href = './page2.html';
+}
